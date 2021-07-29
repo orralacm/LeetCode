30ca7c54c9bf6935ebc1834a78e1f1ef627a0d8f
Mauricio Orrala <orrala@live.com.mx>
2021-07-28 21:30:37 -0600

    problem 14 wip

--- Problem 14/problem_14.py
+++ Problem 14/problem_14.py
@@ -3,23 +3,32 @@ strs = ["flower", "flow", "flight"]
 strs.sort(key=len)
 output = ""
 base = strs[0]
+l = len(base)
 
 print(strs)
 
 if (strs == 0) :
     print ("")
 
-for i in range (1, len(strs)) :
+#for i in range (0, len(base)) :
+    #for j in range (0, len(strs)-1) :
+        
+
+
+for i in range (1, len(strs)-1) :
     print(base)
     word = strs[i]
     print(word)
     for j in range (0, len(base)) :
-
-        if (output == "") :
-            if (word[j] == base[j]) :
-                output += word[j]
-        elif (output[j] == word[j]) :
-            output += word[j]
+        if (base[j] == word[j]) :
+            output += base[j]
+        else :
+            break
+        
+        print(output)
+    
+    if (output == "") :
+        break
 
 print(output)
-    
+
