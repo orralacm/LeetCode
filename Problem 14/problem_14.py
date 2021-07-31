c9524b45d042afe720ee0c633c94c9b5e0c978aa
Mauricio Orrala <orrala@live.com.mx>
2021-07-30 23:09:27 -0600

    problem 14

--- Problem 14/problem_14.py
+++ Problem 14/problem_14.py
@@ -4,31 +4,27 @@ strs.sort(key=len)
 output = ""
 base = strs[0]
 l = len(base)
+letter = ""
 
 print(strs)
 
-if (strs == 0) :
+if (strs == 0 or len(strs) == 0) :
     print ("")
+if (len(strs) == 1) :
+    print (strs[0])
 
-#for i in range (0, len(base)) :
-    #for j in range (0, len(strs)-1) :
-        
-
-
-for i in range (1, len(strs)-1) :
-    print(base)
-    word = strs[i]
-    print(word)
-    for j in range (0, len(base)) :
-        if (base[j] == word[j]) :
-            output += base[j]
-        else :
-            break
-        
-        print(output)
+for i in range (len(strs[0])) :
+    letter = base[i]
     
-    if (output == "") :
-        break
+    a = 0
+    for j in range (1, len(strs)) :
+        word = strs[j]
+        if (letter == word[i]) :
+            a += 1
+    if (a == len(strs)-1) :
+        output += base[i]
+        print(output)
 
-print(output)
+
+
 
