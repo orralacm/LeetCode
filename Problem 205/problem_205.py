7d47c7ffec8262ca9c48f8616f67d49dac03f3d2
Mauricio Orrala <orrala@live.com.mx>
2021-08-09 22:54:36 -0600

    problem 205

---
+++ Problem 205/problem_205.py
@@ -0,0 +1,13 @@
+#Given two strings s and t, determine if they are isomorphic.
+
+s = "egg"
+t = "add"
+
+if (len(s) == len(t)) :
+    for i in range (len(s)) :
+        
+        if (count(s[i]) > 1) :
+            
+
+else :
+    print("No isomorphic")
