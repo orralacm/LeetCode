86a0304fd637b5d2c3e1db3b97e0a422961fda5d
Mauricio Orrala <orrala@live.com.mx>
2021-08-14 06:51:35 -0600

    problem 228

--- Problem 228/problem_228.py
+++ Problem 228/problem_228.py
@@ -2,14 +2,23 @@
 #Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
 
 nums = [0,1,2,4,5,7]
-output = []
-i = 0
 
-while (i < len(nums)) :
-    if (nums[i+1] == (nums[i]+1)) :
-        a = nums[i]
-        if ()
-        output += [f"{i} -> {i+1}"]
-        print(output)
+
+def summaryRanges(self, nums):
+    if not nums:
+        print([])
+    res, i, start = [], 0, 0
+    while i < len(nums)-1:
+        if nums[i]+1 != nums[i+1]:
+            res.append(self.printRange(nums[start], nums[i]))
+            start = i+1
         i += 1
+    res.append(self.printRange(nums[start], nums[i]))
+    print(res)
+
+def printRange(self, l, r):
+    if l == r:
+        return str(l)
+    else:
+        return str(l) + "->" + str(r)
 
