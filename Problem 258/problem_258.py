7dccf6701cb834dfb1114c06dc0e14bd34474c3e
Mauricio Orrala <orrala@live.com.mx>
2021-08-19 23:47:06 -0600

    problem 258

--- Problem 258/problem_258.py
+++ Problem 258/problem_258.py
@@ -1,11 +1,13 @@
 #Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
 
 num = 38
-n = str(num)
-print(len(n))
-a = 0
 
-while len(n) > 1 :
-    for i in range (len(n)) :
-        a += int(n[i])
-        print(a)
+while (num >= 10):
+    temp = 0
+    while (num > 0):
+        temp += num % 10
+        num /= 10
+    num = temp
+    print(num)
+print(num)
+print(38%10)
