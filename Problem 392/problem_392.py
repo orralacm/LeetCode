c59a2e892604ec5bc5eb34efa1df2e482d900c68
Mauricio Orrala <orrala@live.com.mx>
2021-09-13 23:57:05 -0600

    problem 392

--- Problem 392/problem_392.py
+++ Problem 392/problem_392.py
@@ -1,18 +1,14 @@
 #Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 #A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 
-s = "abc"
+s = "axc"
 t = "ahbgdc"
-tt = list(t)
 
-dic = {}
+i, j = 0, 0
 
-for ch in s :
-    dic[ch] = dic.get(ch, 0) + 1
-    print(dic)
-i = 0
-for ch in t :
-    if t[i] not in dic :
-        tt.pop(i)
-    i += 1
-    print(dic)
+while i < len(s) and j < len(t) :
+    print(i, j)
+    if s[i] == t[j] : i += 1
+    j += 1
+
+print(i == len(s))
